c7a3203f7cf04d2149f6f15dcfca57bc6c946eda
daemunozr <dae.munozr@duocuc.cl>
2026-05-11 23:10:24 -0400

    primer commit

---
+++ src/main/java/com/sivebo/ms_comunas/MsComunasApplication.java
@@ -0,0 +1,13 @@
+package com.sivebo.ms_comunas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsComunasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsComunasApplication.class, args);
+	}
+
+}
